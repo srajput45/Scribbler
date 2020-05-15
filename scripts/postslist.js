185acf306476d08b2fc0a0175d6d6d27f289aa12
srajput45 <43792808+srajput45@users.noreply.github.com>
2020-05-15 20:05:09 +0530

    Modified the postList.html, postlist.script and added the fontawesome folder

--- scripts/postslist.js
+++ scripts/postslist.js
@@ -1,3 +1,9 @@
 function newmodal() {
     $('#signInModal').modal('hide');
 }
+
+function show(id) {
+    var x = id;
+    console.log(x);
+    document.getElementById('post1').style.display = 'none';
+}
